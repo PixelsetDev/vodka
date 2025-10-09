9d64080863b6f9030d4effdad5664572a648b359
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-09 23:16:55 +0100

    Add purchasing

--- routes/packs.js
+++ routes/packs.js
@@ -59,4 +59,40 @@ export function routePacks (app, db) {
             })
         }
     });
+
+    app.get('/packs/purchase', withLogto(config), async (request, response) => {
+        response.setHeader('content-type', 'application/json');
+        const stripe = require('stripe')(process.env.STRIPE_KEY);
+
+        const [row] = await db.execute('SELECT * FROM packs WHERE id = ?', [request.query.pack]);
+
+        if (row.length === 0) {
+            response.send({
+                "code": 404,
+                "data": null,
+            })
+        } else {
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price: 'price_1SGS3N2MqXdUCHxjCrl7ehkO',
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: process.env.APP_HTTP_PROTOCOL + '://' + process.env.APP_BASE + '/packs/purchase/success',
+                cancel_url: process.env.APP_HTTP_PROTOCOL + '://' + process.env.APP_BASE + '/packs/purchase/error',
+            });
+        }
+    });
+
+    app.get('/packs/purchase/success', withLogto(config), async (request, response) => {
+        response.setHeader('content-type', 'text/html');
+        response.send("<b>Success</b>")
+    });
+
+    app.get('/packs/purchase/error', withLogto(config), async (request, response) => {
+        response.setHeader('content-type', 'text/html');
+        response.send("<b>Error</b>")
+    });
 }
