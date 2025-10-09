cac599feadd8067bfda4c3679773fd2aa12e937b
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-09 22:50:17 +0100

    Update

--- routes/packs.js
+++ routes/packs.js
@@ -25,4 +25,35 @@ export function routePacks (app, db) {
             })
         }
     });
+
+    app.get('/packs/owns', withLogto(config), async (request, response) => {
+        response.setHeader('content-type', 'application/json');
+        const urlParams = new URLSearchParams(window.location.search);
+
+        if (request.user.isAuthenticated) {
+            try {
+                const uuid = urlParams.get('uuid');
+                const pack = urlParams.get('pack');
+
+                const [rows] = await db.query("SELECT * FROM purchases WHERE uuid = '"+uuid+"' AND pack = '"+pack+"'");
+                console.log(rows);
+
+                response.send({
+                    code: 200,
+                    data: rows,
+                });
+            } catch (err) {
+                response.send({
+                    code: 500,
+                    data: err.message,
+                });
+            }
+
+        } else {
+            response.send({
+                "code": 403,
+                "data": null,
+            })
+        }
+    });
 }
