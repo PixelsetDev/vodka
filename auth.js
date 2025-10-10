673b456cb6da903825c51defe0174de737668478
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-10 18:19:46 +0100

    Add scopes

--- auth.js
+++ auth.js
@@ -1,8 +1,11 @@
+import {UserScope} from "@logto/express";
+
 export const config = {
     appId: process.env.OIDC_CLIENT_ID,
     appSecret: process.env.OIDC_SECRET,
     endpoint: process.env.APP_HTTP_PROTOCOL+'://'+process.env.OIDC_BASE,
-    baseUrl: process.env.APP_HTTP_PROTOCOL+'://'+process.env.APP_BASE
+    baseUrl: process.env.APP_HTTP_PROTOCOL+'://'+process.env.APP_BASE,
+    scopes: [UserScope.Email, UserScope.Profile, UserScope.Roles]
 };
 
 export function isAuthenticated(user) {
