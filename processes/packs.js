be5b1ff1a9e5ced2c4f45d2de6e4ed468104645b
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-11 09:04:29 +0100

    Update ownership logic.

--- processes/packs.js
+++ processes/packs.js
@@ -1,8 +1,10 @@
 export async function userOwns(db, uuid, packID) {
     try {
-        const [rows] = await db.execute('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID]);
+        const [packs] = await db.execute('SELECT * FROM packs WHERE status = 1 AND default = 1');
+        if (packs.length !== 0) { return true; }
 
-        return rows.length !== 0;
+        const [purchases] = await db.execute('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID]);
+        return purchases.length !== 0;
     } catch (err) {
         return false;
     }
