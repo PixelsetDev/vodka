d652db440f72c9ab73da2c9096fd08bb2a204544
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-11 16:49:29 +0100

    Attempt fix

--- processes/packs.js
+++ processes/packs.js
@@ -3,11 +3,13 @@ export async function userOwns(db, uuid, packID) {
         const packs = await new Promise((res, rej) =>
             db.query('SELECT * FROM packs WHERE status = 1 AND all_owns = 1', (err, rows) => err ? rej(err) : res(rows))
         );
+        console.log(packs);
         if (packs.length !== 0) return true;
 
         const purchases = await new Promise((res, rej) =>
             db.query('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID], (err, rows) => err ? rej(err) : res(rows))
         );
+        console.log(purchases);
         return purchases.length !== 0;
     } catch (err) {
         console.error(err);
