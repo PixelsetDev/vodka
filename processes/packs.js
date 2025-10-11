02ae0b4bf8634763a6e15ddfffcd1d05e0c93968
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-11 09:36:02 +0100

    SQL keyword conflict

--- processes/packs.js
+++ processes/packs.js
@@ -1,6 +1,6 @@
 export async function userOwns(db, uuid, packID) {
     try {
-        const [packs] = await db.execute('SELECT * FROM packs WHERE status = 1 AND default = 1');
+        const [packs] = await db.execute('SELECT * FROM packs WHERE status = 1 AND all_owns = 1');
         if (packs.length !== 0) { return true; }
 
         const [purchases] = await db.execute('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID]);
