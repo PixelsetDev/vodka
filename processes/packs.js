69978c0ceb9e5e15617e44905e6a9ed470a5c5ae
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-20 22:49:18 +0100

    Switch to promise.

--- processes/packs.js
+++ processes/packs.js
@@ -1,13 +1,11 @@
 export async function userOwns(db, uuid, packID) {
     try {
-        const packs = await new Promise((res, rej) =>
-            db.query('SELECT * FROM packs WHERE status = 1 AND all_owns = 1 AND id = ?', [packID], (err, rows) => err ? rej(err) : res(rows))
-        );
+        const [packs] = await db.query('SELECT * FROM packs WHERE status = 1 AND all_owns = 1 AND id = ?', [packID]);
+
         if (packs.length !== 0) return true;
 
-        const purchases = await new Promise((res, rej) =>
-            db.query('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID], (err, rows) => err ? rej(err) : res(rows))
-        );
+        const [purchases] = await db.query('SELECT * FROM purchases WHERE uuid = ? AND pack = ?', [uuid, packID]);
+
         return purchases.length !== 0;
     } catch (err) {
         console.error(err);
