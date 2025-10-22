d35ed356c98e479ef82abe2c88f96305595be22d
Lewis Milburn <63267144+lewmilburn@users.noreply.github.com>
2025-10-22 23:01:54 +0100

    Update JSON

--- processes/activityManager.js
+++ processes/activityManager.js
@@ -6,7 +6,7 @@ export async function getAllActivities(db, uuid, packs) {
     for (const pack of packs) {
         const owns = await userOwns(db, uuid, pack);
         if (owns) {
-            const [rows] = await db.query("SELECT * FROM activities WHERE pack = ?", [pack]);
+            const [rows] = await db.query("SELECT `heading`,`subheading`,`responses`,`skip` FROM activities WHERE pack = ?", [pack]);
 
             activities.push(...rows);
         }
